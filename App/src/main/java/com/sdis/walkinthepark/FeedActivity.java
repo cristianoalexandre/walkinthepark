e26fe437a322fbe972a3ac927fc1baf053ca1e03
Cristiano Rodrigues <cristianoalexandre@gmail.com>
2013-05-18 02:21:36 +0100

    Firt app version. Already registers a route using Android Location.

---
+++ App/src/main/java/com/sdis/walkinthepark/FeedActivity.java
@@ -0,0 +1,12 @@
+package com.sdis.walkinthepark;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FeedActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_feed);
+    }
+}
---
+++ App/src/main/java/com/sdis/walkinthepark/PlanActivity.java
@@ -0,0 +1,12 @@
+package com.sdis.walkinthepark;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlanActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_plan);
+    }
+}
